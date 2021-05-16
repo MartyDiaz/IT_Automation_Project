fc9e000ba1f50a6419e274c1552ae888c49917e2
Martin Diaz <martydiaz01@gmail.com>
2021-05-16 15:29:16 -0700

    Add asserts to make sure mock functions are called
    Mock functions call assert_called() to make sure they are being used in
    test_health_checks

--- tests/test_health_checks.py
+++ tests/test_health_checks.py
@@ -11,13 +11,14 @@ from health_checks import check_cpu, check_disk_space, check_memory, \
 def test_check_cpu(mock_psutil_cpu_percent, _input, expected):
     mock_psutil_cpu_percent.return_value = _input
     assert check_cpu(50) == expected
-
+    mock_psutil_cpu_percent.assert_called()
 
 @pytest.mark.parametrize("_input, expected", [((1000,900,100),False), ((1000,100,900), True)])
 @mock.patch("health_checks.shutil.disk_usage")
 def test_check_disk_space(mock_shutil_disk_usage, _input, expected):
     mock_shutil_disk_usage.return_value = _input
     assert check_disk_space(25) == expected
+    mock_shutil_disk_usage.assert_called()
 
 
 @pytest.mark.parametrize("_input, expected", [(1000, True), (200, False)])
@@ -25,3 +26,4 @@ def test_check_disk_space(mock_shutil_disk_usage, _input, expected):
 def test_check_memory(mock_psutil_virtual_memory, _input, expected):
     mock_psutil_virtual_memory.return_value = mock.Mock(**{"available": _input})
     assert check_memory(500) == expected
+    mock_psutil_virtual_memory.assert_called()
