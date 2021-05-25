67eaa45aa19ef6661ed7b8909f962b21cf72c20b
Martin Diaz <martydiaz01@gmail.com>
2021-05-25 16:14:29 -0700

    Add test for report_email.summary_data()

--- tests/test_report_email.py
+++ tests/test_report_email.py
@@ -1,6 +1,6 @@
 import pytest
 import os
-from report_email import read_data
+from report_email import read_data, summary_data
 
 
 def test_read_data():
@@ -19,3 +19,12 @@ def test_read_data():
     test_weight_list = ['200 lbs', '500 lbs']
     assert (name_list == test_name_list)
     assert (weight_list == test_weight_list)
+
+
+def test_summary_data():
+    name_list = ['Avocado', 'Apple']
+    weight_list = ['200 lbs', '500 lbs']
+    test_summary = 'name: Avocado <br /> weight: 200 lbs <br /><br />name: ' \
+                   'Apple <br /> weight: 500 lbs <br /><br />'
+    summary = summary_data(name_list, weight_list)
+    assert (test_summary == summary)
