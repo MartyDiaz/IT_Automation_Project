4fa934d2a077103ab5147426daf963349dec2603
Martin Diaz <martydiaz01@gmail.com>
2022-05-10 15:25:36 -0700

    Needed to use set, not tuple for no duplicates or sorting

--- tests/test_report_email.py
+++ tests/test_report_email.py
@@ -11,10 +11,10 @@ def test_read_data():
     )
 
     name_list, weight_list = read_data(test_description_directory)
-    name_list = tuple(name_list)
-    weight_list = tuple(weight_list)
-    test_name_list = ('Avocado', 'Apple')
-    test_weight_list = ('200 lbs', '500 lbs')
+    name_list = set(name_list)
+    weight_list = set(weight_list)
+    test_name_list = {'Avocado', 'Apple'}
+    test_weight_list = {'200 lbs', '500 lbs'}
     assert name_list == test_name_list
     assert weight_list == test_weight_list
 
